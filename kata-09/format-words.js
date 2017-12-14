080ca8829ab871c1feb232ba3383096a7e462af1
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-13 18:59:03 -0800

    Finished coding challenge format words.

--- kata-09/format-words.js
+++ kata-09/format-words.js
@@ -1,3 +1,9 @@
 function formatWords(words){
-  
+  if (!words || !words.length) return '';
+  let cleanwords = words.filter(ele => ele != '');
+  let last = '';
+  if (cleanwords.length > 1) {
+    last = ' and ' + cleanwords.pop();
+  }
+  return cleanwords.join(', ') + last;
 }
